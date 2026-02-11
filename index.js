8571148c2e6255cd21cf4cb856e9265126909949
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2026-02-11 14:47:29 +0530

    5

--- index.js
+++ index.js
@@ -13,8 +13,33 @@ import propertyListingRoutes from './src/routes/propertyListing.route.js';
 
 dotenv.config();
 const app = express();
-
-app.use(cors());
+app.set('trust proxy', 1);
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'http://localhost:5174',
+  'http://127.0.0.1:5174',
+  'https://www.patilbars.in',
+  'https://patilbars.in',
+  'https://www.patilassociates.in',
+  'https://patilassociates.in',
+  'https://elite-cards-admin-panel.vercel.app'
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 
 // Body parser middleware
 app.use(express.json({ limit: '10mb' }));
